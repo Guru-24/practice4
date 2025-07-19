0a76d2b37aacbabb22af6b1239fd5a9ba8a2e659
Guru-24 <guru.g@s.amity.edu>
2025-07-19 19:37:33 +0530

    added a class name with taskmanager and defined some classes in it

--- prac4.py
+++ prac4.py
@@ -13,4 +13,15 @@ class Task:
     @staticmethod
     def from_dict(data):
         return Task(data["description"], data["completed"])
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+        self.load_tasks()
+
+    def load_tasks(self):
+        if os.path.exists(TASKS_FILE):
+            with open(TASKS_FILE, 'r') as file:
+                data = json.load(file)
+                self.tasks = [Task.from_dict(d) for d in data]
+
 
