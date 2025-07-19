495b1b87ff1581789d758e5ba3b523fe6c22b6c8
Guru-24 <guru.g@s.amity.edu>
2025-07-19 19:36:06 +0530

    added a class name with task

--- prac4.py
+++ prac4.py
@@ -2,4 +2,15 @@ import json
 import os
 
 TASKS_FILE = "tasks.json"
+class Task:
+    def __init__(self, description, completed=False):
+        self.description = description
+        self.completed = completed
+
+    def to_dict(self):
+        return {"description": self.description, "completed": self.completed}
+
+    @staticmethod
+    def from_dict(data):
+        return Task(data["description"], data["completed"])
 
